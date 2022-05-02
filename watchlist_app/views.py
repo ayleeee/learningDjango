fb4cdbc798864dd244fcb5496e5e0fb35f6cf13b
aylee <miiia@khu.ac.kr>
2022-05-03 00:17:53 +0900

    Implemented  RESTful API

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,25 +1,24 @@
-from django.shortcuts import render
-from watchlist_app.models import Movie
-from django.http import JsonResponse
+#from django.shortcuts import render
+#from watchlist_app.models import Movie
+#from django.http import JsonResponse
 
-def movie_list(request):
-    movies = Movie.objects.all() ## all the objects that are availiable
-    data={
-        'movies':list(movies.values())
-        }
+#def movie_list(request):
+    #movies = Movie.objects.all() ## all the objects that are availiable
+    #data={
+        #'movies':list(movies.values())
+       # }
     
     ## JsonResponse will convert python-dictionary to JSON format
-    
-    return JsonResponse(data)
+    #return JsonResponse(data)
     
     
 
-def movie_details(request,pk):
-    movie = Movie.objects.get(pk=pk)
-    data={
-        'name':movie.name,
-        'description':movie.description,
-        'active': movie.active,
+#def movie_details(request,pk):
+    #movie = Movie.objects.get(pk=pk)
+    #data={
+     #   'name':movie.name,
+      #  'description':movie.description,
+       # 'active': movie.active,
     }
     #print(movie.name)
-    return JsonResponse(data)
+ #   return JsonResponse(data)
